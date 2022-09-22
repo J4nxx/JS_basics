a3cc07bab23815753dc8ac52c263c5bdcb64c7a6
Janre Liiv <janre.liiv@khk.ee>
2022-09-22 13:44:17 +0300

    Condition processing

--- app.js
+++ app.js
@@ -1,21 +1,69 @@
-const today = new Date()
+let personID = 34;
 
-let val
+if (personID === '34'){
+    console.log('Id is not correct')
+}  else {
+    console.log(`id is ${personID}`)
+}
 
-val = today
-val = today.getMonth() //0 = January
-val = today.getDate()
-val = today.getDay()
-val = today.getFullYear()
-val = today.getHours()
-val = today.getMinutes()
-val = today.getTime()
+console.log(personID)
 
-val = new Date(09/22/2021)
-val = new Date('September 23 2022')
 
-val = new Date()
-val.setDate(23)
-val.setFullYear(2023)
+/*const color = 'red'
 
-console.log(val)
+if(color === 'red'){
+    console.log('STOP!')
+} else if(color === 'yellow'){
+    console.log('Attention!')
+} else if(color === 'green'){
+    console.log('GO!')
+} else {
+    console.log('Eroor!')
+}
+
+/*
+
+condition1 && condition2 - and
+condition2  condition2 - or
+ * */
+
+const name = 'Kadi'
+const age = 40
+
+if(age > 0 && age<=12){
+    console.log(`${name} on laps`)
+} else if(age > 12 && age <18){
+    console.log(`${name} on nooruk`)
+} else {
+    console.log(`${name} on taiskasvanu`)
+}
+
+let id = 5
+
+if(id ===100){
+    console.log('Ok')
+} else {
+    console.log('Not')
+}
+
+// ? - ternary
+console.log(id ===100 ? 'Ok' : 'Not')
+
+// switch
+const color = 'red'
+
+switch (color) {
+    case "red":
+        console.log('stop!')
+        break;
+    case "yellow":
+        console.log('Attention!')
+        break;
+    case "green":
+        console.log('Go!')
+        break;
+    default:
+        console.log('Error!')
+}
+
+
