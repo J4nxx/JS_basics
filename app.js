894d1baa1d2488b7a434cc4887ac003eb2b90233
Janre Liiv <janre.liiv@khk.ee>
2022-09-22 13:06:06 +0300

    Objects and attributes

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
-const numbers1 = [63,46,52,52,36,62,5]
-const numbers2 = new Array()
+const person = {
+    firstname: = "Kadi",
+    surname: "Tamm",
+    age: 40,
+    email: "kadi.tamm@gmail.com",
+    hobbies: ["tennis", "korvapall"],
+    address: {
+      city: "Voru"
+      county: "Vorumaa"
+    },
+    getBirthYear: function(){
+        return 2022 - this.age
+    }
+}
 
 let val
-
-val = numbers1.length
-val = numbers2.length
-
-val = numbers1[3]
-val = numbers1.indexOf(36)
-
-val = Array.isArray(numbers2)
-
-numbers1.push(250)
-numbers1.pop()
-numbers1.unshift(120)
-numbers1.shift()
-
-val = numbers1.concat(numbers2)
-
-const fruits = ['banana', 'apple', 'orange']
-val = fruits.sort()
-
-val = numbers1.sort(function(x, y) {
-    return y-x
-
-})
-
+val = person
+val = person.firstname
+val = person.surname
+val = person.getBirthYear()
+val = person.hobbies[0]
+val = person.city
+val = person.address.city
+val = person.address['city']
 console.log(val)
+
+for(let i = 0; i < person.hobbies.length; i++){
+    console.log(person.hobbies[1])
+}
+
+person.hobbies.forEach(hobbie:string, index: number) => {
+    console.log(hobbie)
+}
+
