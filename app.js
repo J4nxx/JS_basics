d3df3f493184015acce7d80796d0d4fcdb5475cd
Janre Liiv <janre.liiv@khk.ee>
2022-09-29 11:12:55 +0300

    As a user I can delete a single task [Delivers #183413651]

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
-const form = document.querySelector("form")
-const taskInput = document.querySelector("#task")
+const form  = document.querySelector('form')
+const taskInput = document.querySelector('#task')
+const taskList = document.querySelector('ul')
+
 form.addEventListener('submit', addTask)
+taskList.addEventListener('click', deleteTask)
+
 function addTask(e){
-    console.log(taskInput.value)
+    // create list item
     const li = document.createElement('li')
     li.appendChild(document.createTextNode(taskInput.value))
     li.className = 'collection-item'
     const a = document.createElement('a')
     a.appendChild(document.createTextNode('X'))
     a.className = 'blue-text text-darken-2 secondary-content'
-    a.setAttribute('x')
+    a.setAttribute('href', '#')
     li.appendChild(a)
-    //add to list
+    // add to list
     const ul = document.querySelector('ul')
     ul.appendChild(li)
     taskInput.value = ''
     e.preventDefault()
 }
 
+function deleteTask(e){
+    if(e.target.textContent == 'X'){
+        if(confirm('Are you sure you want to delete this task?')){
+            e.target.parentElement.remove()
+        }
+    }
+
+}
